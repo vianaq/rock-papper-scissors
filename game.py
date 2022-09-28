6678a4c5bbaad732270f838c6d77b53c6531e639
vianaq <114050664+vianaq@users.noreply.github.com>
2022-09-28 08:20:41 +0300

    Update game.py

--- game.py
+++ game.py
@@ -11,3 +11,28 @@ elif human_turn == 'scissors' and computer_turn == 'paper' :
     print('Human wins!')
 else:
     print('Computer wins!')
+    
+    
+    import random
+turns = ['rock', 'paper', 'scissors']
+
+while(True):
+ human_turn = input("Enter your turn, or type exit: ")
+ computer_turn = random.choice(turns)
+
+ if human_turn == 'exit':
+    print('Thank u for playing bye')
+    break
+
+ print(f'Human:{human_turn} vs. Computer:{computer_turn}')
+ if human_turn == computer_turn :
+    print('Draw!')
+ elif human_turn == 'rock' and computer_turn == 'scissors' :
+    print('Human wins!')
+ elif human_turn == 'paper' and computer_turn == 'rock' :
+    print('Human wins!')
+ elif human_turn == 'scissors' and computer_turn == 'paper' :
+    print('Human wins!')
+ else:
+    print('Computer wins!')
+
